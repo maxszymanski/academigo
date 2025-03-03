c30d66bc3013c668107755cf8b733572d4c461b4
Max Szymański <maksymilianszymanski2@gmail.com>
2025-03-03 07:17:02 +0100

    fix errors

--- src/app/_components/_panel/Notifications.tsx
+++ src/app/_components/_panel/Notifications.tsx
@@ -1,7 +1,3 @@
-import Link from 'next/link'
-import { FaPlus } from 'react-icons/fa6'
-import NotificationList from './NotificationList'
-
 function Notifications() {
     return (
         <div className="px-5 pt-12 md:pt-6 xl:rounded-3xl xl:bg-white xl:px-8 xl:py-8 2xl:px-20">
--- src/app/_components/_ui/AuthButton.tsx
+++ src/app/_components/_ui/AuthButton.tsx
@@ -1,4 +1,3 @@
-import { auth } from '@/app/_lib/auth'
 import Button from './Button'
 
 function AuthButton() {
