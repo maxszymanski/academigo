4d85083ade3ae84619ed4f3025e29ef44ccf82ae
Max Szymański <maksymilianszymanski2@gmail.com>
2025-05-16 13:46:59 +0200

    chacge sendEmail secure

--- src/app/_components/_panel/AccountInformation.tsx
+++ src/app/_components/_panel/AccountInformation.tsx
@@ -20,34 +20,36 @@ function AccountInformation({ user, isGoogleAccount }: { user: User; isGoogleAcc
 
 	const formatted = formattedDate(user.created_at)
 	return (
-		<SettingsBox title="Informacje o koncie">
-			<div className="flex flex-col gap-5 h-full">
-				<div className="text-dark2/80 text-sm md:text-base flex flex-col sm:flex-row flex-wrap gap-x-3 gap-y-1">
-					<p>Właściciel:</p>
-					<p className=" text-dark2 ">{user.user_metadata.name}</p>
-				</div>
-				<div className=" text-sm md:text-base text-dark2/80 flex flex-col sm:flex-row flex-wrap gap-x-3 gap-y-1 ">
-					<p>Data utworzenia:</p> <p className="  text-dark2">{formatted}</p>
-				</div>
-				<div className="text-dark2/80 text-sm md:text-base flex flex-col sm:flex-row flex-wrap gap-x-3 gap-y-1">
-					<p>Email:</p>
-					<p className=" text-dark2 ">{user.email}</p>
-				</div>
-				<div className="text-dark2/80 text-sm md:text-base flex flex-col sm:flex-row flex-wrap gap-x-3 gap-y-1">
-					<p>Konto Google:</p>
-					<p className=" text-dark2 ">{isGoogleAccount ? 'Połączone' : 'Nie Połączone'}</p>
-				</div>
+		<>
+			<SettingsBox title="Informacje o koncie">
+				<div className="flex flex-col gap-5 h-full">
+					<div className="text-dark2/80 text-sm md:text-base flex flex-col sm:flex-row flex-wrap gap-x-3 gap-y-1">
+						<p>Właściciel:</p>
+						<p className=" text-dark2 ">{user.user_metadata.name}</p>
+					</div>
+					<div className=" text-sm md:text-base text-dark2/80 flex flex-col sm:flex-row flex-wrap gap-x-3 gap-y-1 ">
+						<p>Data utworzenia:</p> <p className="  text-dark2">{formatted}</p>
+					</div>
+					<div className="text-dark2/80 text-sm md:text-base flex flex-col sm:flex-row flex-wrap gap-x-3 gap-y-1">
+						<p>Email:</p>
+						<p className=" text-dark2 ">{user.email}</p>
+					</div>
+					<div className="text-dark2/80 text-sm md:text-base flex flex-col sm:flex-row flex-wrap gap-x-3 gap-y-1">
+						<p>Konto Google:</p>
+						<p className=" text-dark2 ">{isGoogleAccount ? 'Połączone' : 'Nie Połączone'}</p>
+					</div>
 
-				<div className="flex items-center justify-center mt-auto">
-					<Button
-						variant="transparentDark"
-						restClass="p-0.5 !text-red-600 hover:!text-red-400"
-						onClick={handleOpenDeleteAvatarModal}>
-						Usuń konto
-					</Button>
+					<div className="flex items-center justify-center mt-auto">
+						<Button
+							variant="transparentDark"
+							restClass="p-0.5 !text-red-600 hover:!text-red-400"
+							onClick={handleOpenDeleteAvatarModal}>
+							Usuń konto
+						</Button>
+					</div>
 				</div>
-			</div>
-		</SettingsBox>
+			</SettingsBox>
+		</>
 	)
 }
 
